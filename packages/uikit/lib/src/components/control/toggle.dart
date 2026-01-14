c6085b4bef41c9ffaf19d4c96303557269ad7f4f
MAD(Mobile) <madmobile@iMac-Pro--5-VIDEO-2025.local>
2026-01-14 18:46:53 +0300

    one

--- packages/uikit/lib/src/components/control/toggle.dart
+++ packages/uikit/lib/src/components/control/toggle.dart
@@ -5,18 +5,14 @@ class Toggle extends StatefulWidget {
   final bool isActive;
   final ValueChanged<bool>? onChanged;
 
-  const Toggle({
-    super.key,
-    this.isActive = false,
-    this.onChanged,
-  });
+  const Toggle({super.key, this.isActive = false, this.onChanged});
 
   @override
   State<Toggle> createState() => _ToggleState();
 }
 
 class _ToggleState extends State<Toggle> {
-  bool _isActive = false;
+  late bool _isActive;
 
   @override
   void initState() {
@@ -27,46 +23,90 @@ class _ToggleState extends State<Toggle> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          _isActive = !_isActive;
-        });
+      onTap: (){
+        setState(() => _isActive=!_isActive);
         widget.onChanged?.call(_isActive);
       },
       child: Container(
-        width: 48,
-        height: 28,
+        height: 48,
+        width: 28,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(14), // 28/2 = 14
-          color: _isActive ? ui.color.accent : Color(0xFFEBEBEB),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(2),
-          child: Align(
-            alignment: _isActive ? Alignment.centerRight : Alignment.centerLeft,
-            child: Container(
-              width: 24,
-              height: 24,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.06), // 6%
-                    blurRadius: 1,
-                    offset: Offset(0, 3),
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.15), // 15%
-                    blurRadius: 8,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          borderRadius: BorderRadius.circular(14)
         ),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:uikit/uikit.dart';
+//
+// class Toggle extends StatefulWidget {
+//   final bool isActive;
+//   final ValueChanged<bool>? onChanged;
+//
+//   const Toggle({
+//     super.key,
+//     this.isActive = false,
+//     this.onChanged,
+//   });
+//
+//   @override
+//   State<Toggle> createState() => _ToggleState();
+// }
+//
+// class _ToggleState extends State<Toggle> {
+//   bool _isActive = false;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _isActive = widget.isActive;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         setState(() {
+//           _isActive = !_isActive;
+//         });
+//         widget.onChanged?.call(_isActive);
+//       },
+//       child: Container(
+//         width: 48,
+//         height: 28,
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(14), // 28/2 = 14
+//           color: _isActive ? ui.color.accent : Color(0xFFEBEBEB),
+//         ),
+//         child: Padding(
+//           padding: EdgeInsets.all(2),
+//           child: Align(
+//             alignment: _isActive ? Alignment.centerRight : Alignment.centerLeft,
+//             child: Container(
+//               width: 24,
+//               height: 24,
+//               decoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 color: Colors.white,
+//                 boxShadow: [
+//                   BoxShadow(
+//                     color: Colors.black.withOpacity(0.06), // 6%
+//                     blurRadius: 1,
+//                     offset: Offset(0, 3),
+//                   ),
+//                   BoxShadow(
+//                     color: Colors.black.withOpacity(0.15), // 15%
+//                     blurRadius: 8,
+//                     offset: Offset(0, 3),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
